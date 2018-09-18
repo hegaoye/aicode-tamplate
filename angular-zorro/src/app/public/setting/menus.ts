38f08dfd762ae8a1f07efbe56c840e52f1f78689
jane-gao <gaojieprincess@gmail.com>
2018-09-18 20:08:02 +0800

    up template

--- angular-zorro/src/app/public/setting/menus.ts
+++ angular-zorro/src/app/public/setting/menus.ts
@@ -12,7 +12,7 @@ const suppliers: MenuItem = {
   subMenuList: [
     {
       menuName: "MenuSupplier0001",
-      menuUrl: '/main/supplier/${classNameLower}'
+      menuUrl: '/main/supplier/${module}'
     }
   ]
 };
