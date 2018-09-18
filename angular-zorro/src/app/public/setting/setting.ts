5aa360c1c47c292e83e19cb5bcab9a075b0cf4b3
jane-gao <gaojieprincess@gmail.com>
2018-09-18 20:23:31 +0800

    up template

--- angular-zorro/src/app/public/setting/setting.ts
+++ angular-zorro/src/app/public/setting/setting.ts
@@ -14,7 +14,8 @@ export class Setting {
     copyright: '© 2018 - 玺得科技',
     logo: '../../../assets/img/logo.png',
     logoDark: '../../../assets/img/logo-dark.png',
-    defaultImg: '../../../assets/img/dummy.png',
+    defaultImg: '',
+    // defaultImg: '../../../assets/img/dummy.png',
     userDefaultImg: '../../../assets/img/user-default.png',
     refundImg: '../../../assets/img/refund.png',
     contactInformation: {
