86a45eed5804bc35baba0298bc03fcea095ba4bf
jane-gao <gaojieprincess@gmail.com>
2018-09-19 17:15:23 +0800

    up 替换url

--- angular-template/src/app/public/setting/setting_url.ts
+++ angular-template/src/app/public/setting/setting_url.ts
@@ -27,28 +27,15 @@ export class SettingUrl {
       getSms: '/sms/forgetPasswordSMS',//（post）获取验证码
       checkSmsCode: '/sms/checkSmsCode',//（get）验证码的校验
     },
-    supplier: {
-      list: '/supplier/list',//供应商列表
-      add: '/supplier/build',//添加供应商
-      modify: '/supplier/modify',//修改供应商
-      load: '/supplier/load/code/',//查询供应商信息
-      updateState: '/supplier/updateState',//修改供应商状态
-    },
-    supplierCategory: {
-      list: '/supplierCategory/list',//供应商类型列表
-      add: '/supplierCategory/build',//添加供应商类型
-      modify: '/supplierCategory/modify',//修改供应商类型
-      load: '/supplierCategory/load/code/',//查询供应商类型信息
-    },
-    payment: {
-      list: '/paymentTerm/list',//支付方式列表
-      add: '/paymentTerm/build',//添加支付方式
-      modify: '/paymentTerm/modify',//修改支付方式
-      load: '/paymentTerm/load/code/',//查询支付方式信息
-    },
-    enums: {
-      gender: '/res/enum/',//性别枚举
-    },
+    <#list classes as class>
+  ${class.classModel}: {
+        list: '/${class.classModel}/list',//${class.classModel}列表
+        add: '/${class.classModel}/build',//添加${class.classModel}
+        modify: '/${class.classModel}/modify',//修改${class.classModel}
+        load: '/${class.classModel}/load/code/',//查询${class.classModel}信息
+        updateState: '/${class.classModel}/updateState',//修改${class.classModel}状态
+      },
+    </#list>
   };
 
 // 路由链接信息
@@ -59,11 +46,13 @@ export class SettingUrl {
     pass: {
       login: "/listData/login", //登录
     },
-    supplier: {
-      list: '/main/${classNameLower}/list',//列表
-      add: '/main/${classNameLower}/add',//添加
-      modify: '/main/${classNameLower}/modify',//修改
-      detail: '/main/${classNameLower}/detail',//详情
-    }
+  <#list classes as class>
+${class.classModel}: {
+      list: '/main/${class.classModel}/list',//${class.classModel}列表
+      add: '/main/${class.classModel}/add',//添加${class.classModel}
+      modify: '/main/${class.classModel}/modify',//修改${class.classModel}
+      detail: '/main/${class.classModel}/detail',//查询${class.classModel}信息
+    },
+    </#list>
   }
 }
