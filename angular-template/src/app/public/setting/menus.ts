de667424c66e9f74c66eda6ecdce15259306aca9
jane-gao <gaojieprincess@gmail.com>
2018-09-20 10:47:31 +0800

    up template

--- angular-template/src/app/public/setting/menus.ts
+++ angular-template/src/app/public/setting/menus.ts
@@ -5,16 +5,14 @@ import {MenuItem} from "./model";
  * 本地开发使用菜单
  * 生产环境中菜单信息将由登录接口返回
  */
-const suppliers: MenuItem = {
-  menuName: "MenuSupplier0000",
+<#list classes as class>
+const ${class.classModel}: MenuItem = {
+  menuName: "${class.classModel}",
   menuIcon: 'anticon anticon-shop',
-  menuUrl: '/main/supplier',
-  subMenuList: [
-    {
-      menuName: "MenuSupplier0001",
-      menuUrl: '/main/${classNameLower}'
-    }
-  ]
-};
-export const MENUS = [suppliers];
+  menuUrl: '/main/${class.classModel}'
+},
+</#list>
+export const MENUS = [
+  <#list classes as class>${class.classModel},</#list>
+];
 
