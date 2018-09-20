cac92c0ff650812ce30eda8ac8f216ea7b1c3e7a
jane-gao <gaojieprincess@gmail.com>
2018-09-20 15:52:14 +0800

    up template

--- angular-template/src/app/public/pipes/enum-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EnumNamePipe } from './enum-name.pipe';
-
-describe('EnumNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new EnumNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- angular-template/src/app/public/pipes/splice-str.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SpliceStrPipe } from './splice-str.pipe';
-
-describe('SpliceStrPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SpliceStrPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
