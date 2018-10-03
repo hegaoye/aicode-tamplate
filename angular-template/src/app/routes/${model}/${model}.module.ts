1b92bb35bdd117871a8bc9c128eb2129ea26c59e
jane-gao <gaojieprincess@gmail.com>
2018-10-03 15:50:03 +0800

    up 优化模板

--- angular-template/src/app/routes/${model}/${model}.module.ts
+++ angular-template/src/app/routes/${model}/${model}.module.ts
@@ -11,7 +11,7 @@ import {${class.className}EditComponent} from "./${class.dashedCaseName}-edit/${
 const routes: Routes = [
   <#list modelClasses as class>
     <#if modelClasses?size gt 1>
-    <#if modelClasses?size == 1>{path: '', redirectTo: ' ${class.dashedCaseName}'},</#if>
+    <#if class_index == 0>{path: '', redirectTo: ' ${class.dashedCaseName}'},</#if>
     {path: '${class.dashedCaseName}', children: [
     </#if>
         {path: '', redirectTo: 'list'},
@@ -23,7 +23,7 @@ const routes: Routes = [
     ]}<#if class_has_next>,</#if>
     </#if>
   </#list>
-]
+];
 
 @NgModule({
   imports: [
