6c8c27ec0f2537e48c6f373d37825dab171ca9b0
jane-gao <gaojieprincess@gmail.com>
2018-10-30 17:10:33 +0800

    up 模板

--- angular-template/src/app/public/pipes/date-en.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateEnPipe } from './date-en.pipe';
-
-describe('DateEnPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateEnPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- angular-template/src/app/public/service/ajax.service.ts
+++ angular-template/src/app/public/service/ajax.service.ts
@@ -1,13 +1,19 @@
 import {Injectable} from "@angular/core";
-import {Util} from "../util/util";
 
 import * as $ from "jquery";
+import {RequestFilterService} from "./request-filter.service";
 
 @Injectable()
 export class AjaxService {
-  constructor() {
+  constructor(private requestFilterService: RequestFilterService) {
   }
 
+  /**
+   * mask: false,//是否需要显示遮罩层
+   * auth: false,//是否需要权限验证
+   * shtml: false,//是否需要添加.shtml后缀
+   * @param config
+   */
   //get方式提交，一般用于查询
   public get(config) {
     if (!config) {
@@ -24,35 +30,7 @@ export class AjaxService {
       console.log('ajax调用参数不能为空');
       return;
     }
-    config.url += ".shtml"; //为所有请求带上后缀
-    let async = true, method = 'post', dataType = 'json';
-    if (!config.hasOwnProperty('async')) config.async = async;
-    if (!config.method) config.method = method;
-    if (!config.dataType) config.dataType = dataType;
-
-
-    //提交前显示遮罩层
-    config.beforeSend = function (xhr) {
-      if (config.mask === true) Util.showMask();//显示遮罩层
-    };
-
-    //设置全局ajax登录拦截
-    let success = config.success;
-    config.success = function (result, status, xhr) {
-      if (config.mask === true) Util.hideMask();//隐藏遮罩层
-      //过滤登录，登陆过期或没有登陆
-      if (xhr.getResponseHeader("serverError") || xhr.getResponseHeader("serverError") === "sessionOut") {
-        // window.location.href = SettingUrl.ROUTERLINK.pass.login; //去往登录页面
-      } else {
-        if (typeof success === "function") success(result, status, xhr);
-      }
-    };
-    let error = config.error;
-    config.error = function (result, status, xhr) {
-      if (config.mask === true) Util.hideMask(); //隐藏遮罩层
-      //回调
-      if (typeof error === 'function') error(result, status, xhr);
-    };
+    config = this.requestFilterService.requestConfigFilter(config);
     $.ajax(config);
   };
 
