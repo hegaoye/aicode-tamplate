a76c816cc585042cfcea98ea91ed0ac9703886fc
jane-gao <gaojieprincess@gmail.com>
2019-01-17 20:43:20 +0800

    up 模板angular-template-i18n

--- angular-template-i18n/src/app/shared/shared.module.ts
+++ angular-template-i18n/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import {ModuleWithProviders, NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 import {NgZorroAntdModule} from "ng-zorro-antd";
 import {ImgSizePipe} from "../public/pipes/img-size.pipe";
 import {EnumNamePipe} from "../public/pipes/enum-name.pipe";
@@ -13,6 +14,7 @@ import {BackBtnComponent} from "./compontents/back-btn.component";
     CommonModule,                 //核心模块，必须
     FormsModule,                  //表单支持
     RouterModule,           //路由依赖模块
+    TranslateModule,           //国际化
     ReactiveFormsModule,         //表单支持
     NgZorroAntdModule,            //zorro
   ],
@@ -27,6 +29,7 @@ import {BackBtnComponent} from "./compontents/back-btn.component";
     RouterModule,           //路由依赖模块
     FormsModule,            //表单支持
     ReactiveFormsModule,    //表单支持
+    TranslateModule,            //国际化
     NgZorroAntdModule,            //zorro
     ImgSizePipe,              //图片尺寸
     ImgPreviewPipe,           //图片预览
