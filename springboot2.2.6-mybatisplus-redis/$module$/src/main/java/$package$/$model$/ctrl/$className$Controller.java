6713df295a09c9fe04a4604f98b0363a08817641
bruce <brucejava@subo.com>
2021-02-23 18:45:55 +0800

    增加gateway 项目模板

--- springboot2.2.6-mybatisplus-redis/$module$/src/main/java/$package$/$model$/ctrl/$className$Controller.java
+++ springboot2.2.6-mybatisplus-redis/$module$/src/main/java/$package$/$model$/ctrl/$className$Controller.java
@@ -61,7 +61,7 @@ public class $className$Controller {
                 /***
                  for(field in fields){
                  ***/
-                .eq($className$::get$field.upper$, $className$SaveVO.get$field.upper$())/***}***/);
+                .eq($className$::get$field.upper$, $classNameLower$SaveVO.get$field.upper$())/***}***/);
         if (count > 0) {
             throw new $className$Exception(BaseException.BaseExceptionEnum.Exists);
         }
