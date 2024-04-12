13ee9599b8ddd518ec29588d3cb8c639b1a5c739
abc <abc@gmail.com>
2024-04-12 13:20:24 +0800

    增加mq延迟消息头部支持

--- springboot2.6.5-mybatisplus-redis-rocketmq/$module$/src/main/java/$package$/$model$/mq/producer/$className$SenderImpl.java
+++ springboot2.6.5-mybatisplus-redis-rocketmq/$module$/src/main/java/$package$/$model$/mq/producer/$className$SenderImpl.java
@@ -13,6 +13,7 @@ import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.util.MimeTypeUtils;
 
 
@@ -33,12 +34,16 @@ public class $className$SenderImpl implements $className$Sender {
      */
     @Override
     public void build($className$ $classNameLower$) {
+        log.info("创建 $notes$ $className$ 消息: {}", $classNameLower$);
+
         try {
             String linkId = RandomStringUtils.random(32, true, true);
+            log.info("创建 $notes$ $className$ 消息 linkId: {}", linkId);
+
             $className$Message $classNameLower$Message = new $className$Message();
             BeanUtils.copyProperties($classNameLower$, $classNameLower$Message);
 
-            MessageBuilder messageBuilder = MessageBuilder.withPayload($classNameLower$Message)
+            Message<$classNameLower$Message> message = MessageBuilder.withPayload($classNameLower$Message)
                     .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
                     /**
                      * 延迟消息，必须设置延迟级别，每个级别代表一个延迟时间长度
@@ -46,11 +51,14 @@ public class $className$SenderImpl implements $className$Sender {
                      *延迟：1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
                      */
                     //.setHeader(MessageConst.PROPERTY_DELAY_TIME_LEVEL, 延迟级别)
+                    //事务消息开启
+//                    .setHeader(MessageConst.PROPERTY_TRANSACTION_PREPARED, true)
+//                .setHeader(RocketMQHeaders.TAGS, $className$Message.getCode())
                     .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
                     .setHeader(MessageConst.PROPERTY_ORIGIN_MESSAGE_ID, linkId)
-                    .setHeader(MessageConst.PROPERTY_KEYS, linkId);
-//                .setHeader(RocketMQHeaders.TAGS, $className$Message.getCode());
-            streamBridge.send($className$Topic.$className$Event_OUT.topic, messageBuilder.build());
+                    .setHeader(MessageConst.PROPERTY_KEYS, linkId)
+                    .build();
+            streamBridge.send($className$Topic.$className$Event_OUT.topic, messageBuilder);
         } catch (Exception e) {
             log.error("生产 创建 $notes$ 消息-{}", e.getLocalizedMessage(), e);
         }
